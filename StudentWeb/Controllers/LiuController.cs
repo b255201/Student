21682d9b423e5597bf3970c36ca4dafe7d06b42e
RohimChou <Rohim.chou@gmail.com>
2018-06-01 20:11:41 +0800

    Initialize Project

---
+++ StudentWeb/Controllers/LiuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentWeb.Controllers
+{
+    public class LiuController : Controller
+    {
+        // GET: Liu
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
