9c6ec6fb7ac82194de5b9aea78f6cd46ebda1c4c
kai <kai255201@yahoo.com.tw>
2018-07-28 14:15:50 +0800

    內容業動態

--- StudentWeb/App_Start/RouteConfig.cs
+++ StudentWeb/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace StudentWeb
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            //routes.IgnoreRoute("Studentback/{*pathInfo}");
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
